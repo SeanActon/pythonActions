f949731c0b8df89261673cd535fe27e51ccef280
SeanActon <sean.thomas.acton@gmail.com>
2026-08-22 16:11:16 -0400

    actions change

---
+++ main2.py
@@ -0,0 +1,15 @@
+from utils import  numberAdder, print_plays_data
+
+
+#to do list
+#
+# reading from XML
+# python coding 
+# github actions
+
+
+if __name__ == "__main__":
+    print("This is the main entry point of the program.")
+    print_plays_data(False, "plays.xml")
+    print('------------------------------')
+    print_plays_data(False, "newPlays.xml")
